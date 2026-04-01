c14e4a4e3a00a1e04acf7b079bb3ff94f78628d1
danil <danillybchik94@gmail.com>
2026-04-01 14:14:28 +0300

    task_4_1 is solved

--- task_3.py
+++ task_3.py
@@ -10,3 +10,35 @@
     - предоставьте пользователю выбор форматов и путей к файлам;
     - логируйте операции конвертации в файл converter_log.txt.
 """
+import csv
+
+common_sum = 0
+
+print("=" * 45)
+print(f"{'Товар':<15} {'Общая сумма':<15}")
+print("-" * 45)
+
+try:
+    with open("sales.csv", 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        next(reader)  # Пропускаем заголовок
+
+        for row in reader:
+            if not row or all(cell.strip() == '' for cell in row):
+                continue
+
+            product = row[0].strip()
+            quantity = int(row[1].strip())
+            price = int(row[2].strip())
+
+            sum_products = quantity * price
+            print(f"{product:<15} {sum_products:<15}")
+            common_sum += sum_products
+except FileNotFoundError:
+    print("File not found")
+except ValueError:
+    print("Value Error")
+
+print("-" * 45)
+print(f"{'ИТОГО:':<15} {common_sum:<15}")
+print("=" * 45)
